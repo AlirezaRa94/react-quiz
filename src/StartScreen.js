0e3ca11d9c707b0fd7113203608f1f1ef57dffb8
Alireza Raei <alirezaraei17@gmail.com>
2024-07-05 23:40:49 +1000

    feat: Add quiz start functionality and Question component
    - Enhanced `StartScreen` component to dispatch a `start` action, transitioning the app state to `active` when the "Start Quiz" button is clicked.
    - Added a new `Question` component to display quiz questions when the app state is `active`.
    - Updated the `reducer` in `App.js` to handle the `start` action, setting the app status to `active` and enabling the quiz to begin.

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className='start'>
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} questions to test your react mastry</h3>
-      <button className='btn btn-ui'>Start Quiz</button>
+      <button
+        className='btn btn-ui'
+        onClick={() => dispatch({ type: 'start' })}
+      >
+        Start Quiz
+      </button>
     </div>
   );
 }
