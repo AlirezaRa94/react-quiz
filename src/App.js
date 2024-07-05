3c1b89600e3a42c16a257c52274184b571c7f602
Alireza Raei <alirezaraei17@gmail.com>
2024-07-05 23:19:02 +1000

    feat: Implement question fetching and state management in App
    - Integrated `useEffect` and `useReducer` hooks for fetching questions and managing app state.
    - Defined `initialState` and `reducer` to handle question data and app status (`loading`, `error`, `ready`).
    - Implemented asynchronous `fetchQuestions` function within `useEffect` to retrieve questions from local JSON server and dispatch actions based on fetch success or failure.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, status: 'ready', questions: action.payload };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Unhandled action type: ' + action.type);
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+
+    fetchQuestions();
+  }, []);
   return (
     <div className='app'>
       <Header />
