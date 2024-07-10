822edc4117d3186619201cf7913e1ad328695f3b
Alireza Raei <alirezaraei17@gmail.com>
2024-07-10 12:50:29 +1000

    feat: Implement question display and options interaction
    - Added `index` to the app state in `App.js` to track the current question being displayed.
    - Created `Options` component to display the options for the current question, allowing users to interact with them.
    - Integrated `Options` component within `Question` to render the question text and its options dynamically based on the current question data.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -26,7 +27,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ status, questions }, dispatch] = useReducer(reducer, initialState);
+  const [{ status, questions, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -55,7 +59,7 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question />}
+        {status === 'active' && <Question question={questions[index]} />}
       </Main>
     </div>
   );
